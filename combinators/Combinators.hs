b65ea0e90214414f9a5c7f19676c415201b4e7c1
Owen175 <88042260+Owen175@users.noreply.github.com>
2025-11-19 15:04:08 +0000

    Add files via upload

--- combinators/Combinators.hs
+++ combinators/Combinators.hs
@@ -6,21 +6,29 @@ import Data.List (unfoldr)
 data SKExpr = S | K | SKExpr :@ SKExpr
   deriving (Eq, Show)
 
-
 combexpr = S :@ (K :@ S) :@ S :@ K
 
 class Reducible a where
   reduce :: a -> Maybe a
-
+  reduceAll :: a -> [a]
 
 -- Exercise 1
 instance Reducible SKExpr where
     reduce ((K:@x):@_) = Just x
     reduce (((S:@x):@y):@z) = Just (x :@ z :@ (y :@ z))
     reduce (a:@b) = case reduce a of 
-                            Nothing -> Nothing
+                            Nothing -> case reduce b of 
+                              Nothing -> Nothing
+                              Just x -> Just (a:@x)
                             Just x -> Just (x:@b) 
     reduce _ = Nothing
+
+    reduceAll ((K:@x):@y) = x: [K:@ ra :@ y | ra<-reduceAll x] ++ [K:@x:@ra | ra<-reduceAll y]
+    reduceAll (((S:@x):@y):@z) = x :@ z :@ (y :@ z) : [S:@ ra :@ y :@ z | ra<-reduceAll x] ++ [S:@ x :@ ra :@ z | ra<-reduceAll y] ++ [S:@ x :@ y :@ ra | ra<-reduceAll z] 
+    reduceAll (a:@b) = [ra :@ b | ra<-reduceAll a] ++ [a:@ra | ra<-reduceAll b]
+    reduceAll _ = []
+    
+
 
 
 
@@ -29,3 +37,29 @@ eval :: Reducible a => a -> [a]
 eval a = case reduce a of 
     Nothing -> [a]
     Just x -> a:eval x
+
+data Tree a = Node [Tree a] a
+
+
+evalTree a = Node skls a
+    where skls = map evalTree (reduceAll a)
+ 
+flattenEvalTree (Node [] a) = [[a]]
+flattenEvalTree (Node xs c) = map (c:) (concat [flattenEvalTree x | x<-xs])
+
+evalAll :: Reducible a => a -> [[a]]
+evalAll a = (flattenEvalTree . evalTree) a
+
+
+-- evalAll :: Reducible a => a -> [[a]]
+-- evalAll --
+
+-- reduceAll (Just unchanged) ((K:@x):@y) = x: reduceAll (Just (unchanged :@ K)) (x:@y)
+-- reduceAll Nothing ((K:@x):@y) = x: reduceAll (Just K) (x:@y)
+-- reduceAll (Just unchanged) (((S:@x):@y):@z) = (unchanged :@ x :@ z :@ (y :@ z)) : reduceAll (Just (unchanged :@ S)) (x:@y:@z)
+-- reduceAll Nothing (((S:@x):@y):@z) = (x :@ z :@ (y :@ z)) : reduceAll (Just S) (x:@y:@z)
+
+-- reduceAll (Just unchanged) (a:@b) = [t:@b | t<-reduceAll (Just unchanged) a] ++ [t | t<-reduceAll (Just (a:@unchanged)) b]
+-- reduceAll Nothing (a:@b) = [t:@b | t<-reduceAll Nothing a] ++ [t | t<-reduceAll (Just a) b]
+-- reduceAll Nothing a = [a]
+-- reduceAll (Just unchanged) a = [unchanged :@ a]
--- combinators/Optional.hs
+++ combinators/Optional.hs
@@ -6,4 +6,57 @@ i = S:@K:@K
 -- combinator - w x = x x
 w = S:@i:@i
 
---omega = K:@
+-- S X Y Z = X Z (Y Z)
+
+
+omega = w :@ w   -- repeatedly generates w:@w never getting to the rest 
+
+
+-- need a thing n which makes n a b become a (a(a(a(b)))) for n as
+-- can recurse if i have something that converts a b to a(a b)
+
+--a b --> a (a b)
+
+
+
+-- generateN 0 = a:@b
+-- generateN n = 
+--     where (fst:@lst) = generateN (n-1)
+-- false :@ S :@ a :@ a :@ b
+
+-- https://www.cantab.net/users/antoni.diller/brackets/intro.html
+
+-- b = S:@(K:@S):@K
+-- b' = b:@b
+-- c = S:@(b:@b:@S):@(K:@K)
+-- c' = b:@(b:@c):@b
+-- s' = b:@(b:@(b:@S):@S):@K
+
+-- church numeral successor function:
+
+-- λnfx.f (n f x)
+
+-- lambda x . f(nfx)
+-- --> S (lambda x . f) (lambda x . (n f x))
+-- --> S (K f) (S (K (n f)) (S K K))
+-- then lambda f:
+-- --> lambda f . [S (K f) (S (K (n f)) (S K K))]
+-- --> S (K S) (S (K K) (S K K)) lhs S (K K) (S K K) rhs 
+-- = S (S (K S) (S (K K) (S (K n) (S K K)))) (K (S K K))
+
+
+-- T[x] = x
+-- T[M N] = T[M] T[N]
+-- T[λx. M] =
+--   K (T[M])             if x ∉ FV(M)
+--   I                    if M = x
+--   S (T[λx. M1]) (T[λx. M2])   if M = M1 M2
+
+-- lastElem [x] = x
+-- lastElem (x:xs) = lastElem xs
+
+-- successor _ n = S :@(S:@ (K:@ S):@ (S:@ (K:@ K):@ (S:@ (K :@ n):@ (S:@ K:@ K)))):@ (K:@ (S:@ K:@ K))
+
+-- getN n = eval (foldr successor (K:@(S:@K:@K)) [1..n])
+
+
